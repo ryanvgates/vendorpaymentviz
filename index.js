190ef21631791c2ed39f50accfc99c82d2fa507f
Ryan Gates <ryanvgates@gmail.com>
2015-06-06 13:04:19 -0400

    saving progress before trying bubble cloud

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ $(function() {
 
 function InitChart() {
 	// Chart creation code goes here
+	var vis = d3.select("#svgVisualize");
     var xRange = d3.scale.linear()
                 .range([40, 400])
                 .domain([d3.min(sampleData, function(d) {
@@ -41,6 +42,12 @@ function InitChart() {
                   return d.y;
                 })]);
 
+    var xAxis = d3.svg.axis().scale(xRange);
+    var yAxis = d3.svg.axis().scale(yRange).orient("left");
+    /*
+    vis.append("svg:g").call(xAxis).attr("transform", "translate(0,400)");
+    vis.append("svg:g").call(yAxis).attr("transform", "translate(40,0)");
+	*/
 	var circles = vis.selectAll("circle").data(sampleData);
  
 	circles
