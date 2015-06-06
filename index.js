9ae2ddaa82c9967d472a159be609c53b8add870c
Ryan Gates <ryanvgates@gmail.com>
2015-06-06 12:50:48 -0400

    Removed bar chart since we're not doing it

--- index.js
+++ index.js
@@ -26,14 +26,6 @@ $(function() {
 
 function InitChart() {
 	// Chart creation code goes here
-    var vis = d3.select("#svgVisualize");
-    var xRange = d3.scale.linear().range([40, 400]).domain([0,100]);
-    var yRange = d3.scale.linear().range([40, 400]).domain([0,100]);
-    var xAxis = d3.svg.axis().scale(xRange);
-    var yAxis = d3.svg.axis().scale(yRange);
-    vis.append("svg:g").call(xAxis);
-    vis.append("svg:g").call(yAxis);
-
     var xRange = d3.scale.linear()
                 .range([40, 400])
                 .domain([d3.min(sampleData, function(d) {
