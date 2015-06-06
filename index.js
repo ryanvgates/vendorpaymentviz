39032f809af1fb23413c7f276d46b945e7c5b1ad
Ryan Gates <ryanvgates@gmail.com>
2015-06-06 12:24:26 -0400

    saving progress

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var totals = 
+var totals = {};
 
 $.getJSON("https://opendata.demo.socrata.com/resource/ixay-a4di.json", function( data ) {
 	$.each(data, function(key, value) {
-    	console.log(key + "=" + value);
+		console.log(value);
+		
+    	if (totals.vendor_name != null){
+    		totals.amount += value.amount;
+    	}
+    	else{
+
+    	}
 	});
 });
